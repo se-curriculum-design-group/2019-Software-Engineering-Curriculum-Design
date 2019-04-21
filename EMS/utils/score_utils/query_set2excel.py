eee598d910f7737d576847c5a08e0d682957dff9
RonDen <3391131052@qq.com>
2019-04-21 16:02:13 +0800

    编写查询集到excel表格的程序，未完

---
+++ EMS/utils/score_utils/query_set2excel.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+import MySQLdb
+import xlrd
+import xlwt
+from xlwt import Workbook, Worksheet, Row, Column
+from openpyxl.writer.excel import save_virtual_workbook, save_workbook, ExcelWriter
+
+
+base_dir = '../others/'
+xls_file = '2018-2019-1semester.xls'
+
+
+def write_to_excel(n, head_data, records, download_url):
+
+    pass
+
