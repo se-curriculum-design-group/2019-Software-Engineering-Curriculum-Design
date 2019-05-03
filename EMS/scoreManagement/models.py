b8619c66e505ea0c22dcfc78d88b1d3773c5261e
wmdydxr <qwaszx963@yeah.net>
2019-05-03 22:51:15 +0800

    02

--- EMS/scoreManagement/models.py
+++ EMS/scoreManagement/models.py
@@ -106,21 +106,31 @@ class CourseScore(models.Model):
             'sno'
         )
 
-
+# 学生评价表
 class EvaluationForm(models.Model):
-    teaching = models.OneToOneField(to=Teaching, on_delete=models.CASCADE)
-    item1 = models.CharField(max_length=128)
-    item2 = models.CharField(max_length=128)
-    item3 = models.CharField(max_length=128)
-    item4 = models.CharField(max_length=128)
-    item5 = models.CharField(max_length=128)
-    item6 = models.CharField(max_length=128)
-    "该老师认真负责-A"
-    description = models.TextField()
-    is_finish = models.BooleanField()
+    # 三个对象引用
+    student = models.ForeignKey(to=Student, on_delete=models.CASCADE, default=None)
+    course = models.ForeignKey(to=MajorCourses, on_delete=models.CASCADE, default=None)
+    teacher = models.ForeignKey(to=Teacher, on_delete=models.CASCADE, default=None)
+
+    # 八个评分项（A=100 B=90 C=75 D=60 E=50）
+    item1 = models.IntegerField(default=0)
+    item2 = models.IntegerField(default=0)
+    item3 = models.IntegerField(default=0)
+    item4 = models.IntegerField(default=0)
+    item5 = models.IntegerField(default=0)
+    item6 = models.IntegerField(default=0)
+    item7 = models.IntegerField(default=0)
+    item8 = models.IntegerField(default=0)
+    # 评语
+    description = models.TextField(default="无")
+    # 总分，在前端算完返回给后台
+    sum = models.FloatField(default=0)
+    # 是否已提交
+    is_finish = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.teaching)
+        return "-".join([self.student.__str__(), self.course.__str__(), self.teacher.__str__(), str(self.sum)])
 
     class Meta:
         db_table = 'evaluation_form'
--- EMS/scoreManagement/urls.py
+++ EMS/scoreManagement/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
                   path('welcome', views.welcome, name="welcome"),
                   path('score_home_page', views.score_home_page, name="score_home_page"),
                   path('adm_all_course_score', views.adm_all_course_score, name="adm_all_course_score"),
-              ]
+                  path('assess_teacher', views.assess_teacher, name = "assess_teacher"),
+                  path('submit_result', views.submit_result, name = "submit_result"),
+                  path('submit_all', views.submit_all, name="submit_all"),
+
+]
