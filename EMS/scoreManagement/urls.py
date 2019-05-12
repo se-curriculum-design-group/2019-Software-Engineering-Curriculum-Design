d9ba157d9cf77c3c88aa849188be7f9f825a032f
wmdydxr <qwaszx963@yeah.net>
2019-05-12 22:01:15 +0800

    20190512宋文宇
    修改成绩录入

--- EMS/scoreManagement/urls.py
+++ EMS/scoreManagement/urls.py
@@ -8,15 +8,16 @@ app_name = 'scoreManagement'
 urlpatterns = [
     path('welcome', views.welcome, name="welcome"),
     path('score_home_page', views.score_home_page, name="score_home_page"),
+     path('student_score', views.student_score, name="student_score"),
+    path('student_own_study', views.student_own_study, name="student_own_study"),
     path('adm_all_course_score', views.adm_all_course_score,
          name="adm_all_course_score"),
     path('std_view_major_course', views.std_view_major_course,
          name="std_view_major_course"),
     path('std_view_major_plan', views.std_view_major_plan,
          name="std_view_major_plan"),
-    path('_ajax', views._ajax, name="_ajax"),
     path('assess_teacher', views.assess_teacher, name="assess_teacher"),
     path('submit_result', views.submit_result, name="submit_result"),
     path('submit_all', views.submit_all, name="submit_all"),
-
+    path('input_score',views.input_score,name="input_score"),
 ]
